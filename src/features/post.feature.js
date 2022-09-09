e821b2f15f5550f3c40c29d77edc47950743176b
Yanzen <yanwarsolah@gmail.com>
2022-09-09 12:45:10 +0700

    Memoize useCallback for performance

--- src/features/post.feature.js
+++ src/features/post.feature.js
@@ -1,10 +1,15 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { GetPostProvider } from "../providers/post.provider";
 
 export const PostAdmin = () => {
   const [posts, setPosts] = useState([]);
 
+  // const getData = useCallback(() => {
+  //   GetPostProvider().then((response) => setPosts(response.data));
+  // }, []);
+
   useEffect(() => {
+    console.log("iyaas");
     GetPostProvider().then((response) => setPosts(response.data));
   }, []);
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import ReactDOM from "react-dom/client";
 import { BarangManager } from "./features/barang.feature";
 import { PostAdmin } from "./features/post.feature";
 import reportWebVitals from "./reportWebVitals";
+import Todos from "./Todos";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  const addTodo = useCallback(() => {
+    setTodos((t) => [...t, "New Todo"]);
+  }, [todos]);
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <PostAdmin />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
